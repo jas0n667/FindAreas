2df1c385be6df3e77281bd29a1da018a6bd5065a
Alkhan Zhpalaov <azhpalov04@mail.ru>
2023-09-23 17:00:35 +0600

    Class 'Rectangle' is fiiled

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,11 @@
+class Rectangle extends Figure{
+    Rectangle(int d1, int d2){
+        super(d1, d2);
+    }
 
+    void getArea(){
+        System.out.println("Rectangle's area : " + super.area());
+    }
+
+
+}
