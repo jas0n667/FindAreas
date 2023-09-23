85c7fa82ac6c2ccd117d430e7f614d00e95a0526
Alkhan Zhpalaov <azhpalov04@mail.ru>
2023-09-23 17:12:46 +0600

    Class 'Triangle' is filled

--- Triangle.java
+++ Triangle.java
@@ -1,2 +1,19 @@
-public class Triangle {
+public class Triangle extends Figure{
+    double dim3;
+
+    Triangle(double d1, double d2, double d3){
+        super(d1, d2);
+        dim3 = d1;
+    }
+
+    double area(){
+        double halfP = (dim1 + dim2 + dim3) / 2;
+        return Math.sqrt(halfP * (halfP-dim1) * (halfP-dim2) * (halfP-dim3));
+    }
+
+    void getArea(){
+        System.out.println("Triangle's area : " + area());
+    }
+
+
 }
